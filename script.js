958bfe4b75b8ec1ff1c6f24f3eee12c3a3ed94f0
shaurya101 <71705373+shaurya101@users.noreply.github.com>
2021-10-12 19:53:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
+var color;
 const colorOptions=['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'];
 
+
 function newColor(){
-    var color='#';
+    reset();
+    randomColor();
+    document.querySelector("#screen").style.backgroundColor=color;
+    document.querySelector("#hexDisplay").textContent = color;
+}
+
+function reset(){
+    color='#';
+    document.querySelector("#hexDisplay").textContent = '';
+}
+function randomColor(){
     for(let i=0; i<6; i++){
         let index=Math.floor(Math.random()*16);
         color=color+colorOptions[index];
-    }
-    document.querySelector("#screen").style.backgroundColor=color;
-    console.log(color);
+    }//for
 }
